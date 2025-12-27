25b3045321c5d44cf0d65b74430cbc784b30a0f2
PREVEEN KUMAR S <preveen.spk@gmail.com>
2025-12-27 22:03:09 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
   }
 };
 
